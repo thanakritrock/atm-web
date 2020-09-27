5c0344e552cf46126cb42b831eb0815872c0c3a1
Thanakrit Lochanti <thanakrit.lo@ku.th>
2020-09-27 19:41:47 +0700

    add code from lab7- change JDBC to JBA

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 //customer part
@@ -25,11 +26,12 @@ public class CustomerService {
 
     public Customer findCustomer(int id) {
         try {
-            return repository.findById(id);
-        } catch (EmptyResultDataAccessException e) {
+            return repository.findById(id).get();
+        } catch (NoSuchElementException e) {
             return null;
         }
     }
+
     public List<Customer> getCustomers() {
         return repository.findAll();
     }
