7bb1ca7b25197d3273b083476ae0a3027cb99524
Thanakrit Lochanti <thanakrit.lo@ku.th>
2020-09-27 18:44:33 +0700

    insert code from lab7-1 connect to database !!!the one before 5c31a1b is lab6!!!

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -1,41 +1,36 @@
 package th.ac.ku.atm.service;
 
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CustomerService {
 
-    private List<Customer> customerList;
+    private CustomerRepository repository;
 
-    @PostConstruct
-    public void postConstruct() {
-        this.customerList = new ArrayList<>();
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
     }
 
     public void createCustomer(Customer customer) {
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customerList.add(customer);
+        repository.save(customer);
     }
 
-    public List<Customer> getCustomers() {
-        return new ArrayList<>(this.customerList);
-    }
-    private String hash(String pin) {
-        String salt = BCrypt.gensalt(12);
-        return BCrypt.hashpw(pin, salt);
-    }
     public Customer findCustomer(int id) {
-        for (Customer customer : customerList) {
-            if (customer.getId() == id)
-                return customer;
+        try {
+            return repository.findById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
         }
-        return null;
+    }
+    public List<Customer> getCustomers() {
+        return repository.findAll();
     }
 
     public Customer checkPin(Customer inputCustomer) {
@@ -48,6 +43,11 @@ public class CustomerService {
         }
         return null;
     }
+    private String hash(String pin) {
+        String salt = BCrypt.gensalt(12);
+        return BCrypt.hashpw(pin, salt);
+    }
+
 
 
 }
